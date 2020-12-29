932c396761dcf02b9fa80830e64ade0425207d4a
anitalu724 <b06901011@ntu.edu.tw>
2020-12-29 20:59:43 +0800

    20201229_menu_half

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Modal from './components/modal';
-
+import Project from './components/project'
+import {IconContext} from "react-icons";
+import { FcPlus } from "react-icons/fc";
 
 const initList = [
     {
-        id: '0',
-        name: 'folder1'
-    }
+        name: 'folder1',
+        history:'0'
+    },
+    {
+        name: 'folder2',
+        history:'1'
+    },
+    {
+        name: 'folder3',
+        history:'2'
+    },
 ];
+let a = initList.map(pro => (<Project name={pro.name} hist={ pro.history}></Project>))
+console.log(a)
+
 
 
 function Menu(){ 
-    const [list, setList] = React.useState(initList);
+    const [list, setList] = useState(initList);
     
     const modalRef = React.useRef();
     
@@ -21,7 +34,18 @@ function Menu(){
     
     return (
         <div id='menu_container' >
-            <button id='menu_add_btn' onClick={openModal}>+</button>
+            <div style={{ float: 'left', width: '10%', height: '100%', backgroundColor: 'transparent' }}>
+                <button id='menu_add_btn' onClick={openModal}>
+                    <IconContext.Provider value={{color: 'gray', size: '30px'}}>
+                        <FcPlus/>
+                    </IconContext.Provider>
+                </button>
+            </div>
+            <div className='menu_menu' style={{float:'left',width:'80%',height:'100%', padding:'30px'}}>
+                {initList.map(project => (<Project name={project.name} hist={ project.history}></Project>))}
+            </div>
+            <div style={{float:'right',width:'10%',height:'100%',backgroundColor:'transparent'}}></div>
+
             <Modal ref={modalRef}>
                 <span className='menu_modal_span'>Create a project</span>
                 <div className='menu_modal_inputs'>
--- frontend/src/components/project.js
+++ frontend/src/components/project.js
@@ -1,2 +1,42 @@
-import React from 'react'
+import React,{ useState, Component }  from 'react'
+import './project.css'
+import {IconContext} from "react-icons";
+import { BsTrash,BsClockHistory } from "react-icons/bs";
+import { render } from 'react-dom';
 
+
+
+class Project extends Component { 
+    render() { 
+        const { name, hist } = this.props;
+        console.log(name)
+        return (
+            <div className='project_container'>
+                <div className='project_wrapper' style={{ position: 'relative' }}>
+                    <p className='project_title'>{name}</p>
+                    <div className='project_history'>
+                        <IconContext.Provider value={{ color: 'gray', size: '12px', className: 'project_history' }}>
+                            <BsClockHistory /> <span>changed {hist} days ago</span>
+                        </IconContext.Provider>
+                    </div>
+                    <div className='project_btn'>
+                        <button className='project_trash'>  {/*onClick={ Delete} */}
+                            <IconContext.Provider value={{ color: 'gray', size: '16px', className: 'project_icon' }}>
+                                <BsTrash />
+                            </IconContext.Provider>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    // const [title, setTitle] = useState("");
+    // const [history, setHistory] = useState('0');
+    
+    // console.log("file Project.js")
+    // console.log(this.props.name, this.props.hist);
+    // setTitle(this.props.name);
+    // setHistory(this.props.hist);
+    
+}
+export default Project;
