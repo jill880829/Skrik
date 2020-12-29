d1ea2e11d611dde8d42815d6e0c7743c4d50ea62
anitalu724 <b06901011@ntu.edu.tw>
2020-12-29 13:33:31 +0800

    20201229_menu_modal

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -10,28 +10,28 @@ const initList = [
 ];
 
 
-
-
 function Menu(){ 
     const [list, setList] = React.useState(initList);
+    
+    const modalRef = React.useRef();
+    
     const openModal = () => { 
-        console.log('openModal')
+        modalRef.current.openModal();
     }
     
     return (
         <div id='menu_container' >
             <button id='menu_add_btn' onClick={openModal}>+</button>
-            <Modal>
+            <Modal ref={modalRef}>
                 <span className='menu_modal_span'>Create a project</span>
                 <div className='menu_modal_inputs'>
                     Project Name <input className='menu_modal_input' type='text' name='name' id='name' required='true'/> 
                     Collaborators <input className='menu_modal_input'></input>
                 </div>
                 <div className='menu_modal_btns'>
-                    <button className='menu_modal_btn'>Close</button>
-                    <button className='menu_modal_btn'>Confirm</button>
+                    <button className='menu_modal_btn menu_btn_close' onClick={ ()=>modalRef.current.closeModal()}>Close</button>
+                    <button className='menu_modal_btn menu_btn_confirm' onClick={ ()=>modalRef.current.closeModal()}>Confirm</button>
                 </div>
-                
             </Modal>
         </div>
     )
--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React , { forwardRef, useImperativeHandle }from 'react';
+import ReactDOM from 'react-dom';
+
 import './modal.css'
-const Modal = (props) => {
-    const [display, setDisplay] = React.useState(true);
+const Modal = forwardRef((props, ref) => {
+    const [display, setDisplay] = React.useState(false);
+    useImperativeHandle(ref, () => {
+        return {
+            openModal: () => open(),
+            closeModal:() => close()
+        }
+    });
+
     const open = () => {
         setDisplay(true);
     };
     const close = () => {
         setDisplay(false);
     };
-    if (display) { 
-        return (
-        <div className='modal-wrapper'>
-            <div className='modal-backdrop' />
-            <div className='modal-box'>
-                { props.children}
-            </div>
-        </div>
+    if (display) {
+        return ReactDOM.createPortal(
+            <div className={'modal-wrapper'}>
+                <div onClick={close} className={'modal-backdrop'} />
+                <div className={'modal-box'}>
+                    {props.children}
+                </div>
+            </div>, document.getElementById('modal-root')
         )
     }
     return null;
-    
-
-}
-
+});
 export default Modal;
