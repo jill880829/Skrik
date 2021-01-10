c951a30e9d79b254b4a1e3a079073b2ce4a6bab4
hallo1144 <hallo1144@gmail.com>
2021-01-10 20:38:37 +0800

    finish all oauth

--- backend/routes/LoginRouter.js
+++ backend/routes/LoginRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
-// const GithubStrategy = require('passport-github').Strategy
+const GithubStrategy = require('passport-github2').Strategy
 const GoogleStrategy = require('passport-google-oauth2').Strategy
 
 var QueryUser = require('../utils/db/QueryUser')
@@ -39,6 +39,10 @@ if (process.env.FB_APP_ID !== undefined) {
             console.log(profile);
             try {
                 let user = await QueryUser.authFB(profile.id, profile.displayName);
+                if(user.username === undefined) {
+                    console.log("[fb] authFB missing user: undefined.")
+                    return done(null, false);
+                }
                 return done(null, user);
             }
             catch (err) {
@@ -49,7 +53,7 @@ if (process.env.FB_APP_ID !== undefined) {
     ))
     router.get('/fb', passport.authenticate('facebook'));
 
-    router.get('/callback/fb', passport.authenticate('facebook', { successRedirect: 'Menu', failureRedirect: '/Login' }));
+    router.get('/callback/fb', passport.authenticate('facebook', { successRedirect: '/Menu', failureRedirect: '/Login' }));
 }
 
 
@@ -58,23 +62,56 @@ if (process.env.GOOGLE_APP_ID !== undefined) {
             clientID: process.env.GOOGLE_APP_ID,
             clientSecret: process.env.GOOGLE_APP_SECRET,
             callbackURL: process.env.LOGIN_URL + "/callback/google",
-            passReqToCallback   : true
+            passReqToCallback: true
     },
-        async function (accessToken, refreshToken, profile, done) {
+        async function(req, accessToken, refreshToken, profile, done) {
             console.log(profile);
             try {
-                let user = await QueryUser.authFB(profile.id, profile.displayName);
+                let user = await QueryUser.authGoogle(profile.id, profile.displayName);
+                if(user.username === undefined) {
+                    console.log("[google] authGoogle missing user: undefined.")
+                    return done(null, false);
+                }
                 return done(null, user);
             }
             catch (err) {
-                console.log("[fb] unexpected error: " + err)
+                console.log("[google] unexpected error: " + err)
                 return done(null, false)
             }
         }
     ))
-    router.get('/fb', passport.authenticate('facebook'));
+    router.get('/google', passport.authenticate('google', { scope:[ 'profile' ] }));
 
-    router.get('/callback/fb', passport.authenticate('facebook', { successRedirect: 'Menu', failureRedirect: '/Login' }));
+    router.get('/callback/google', passport.authenticate('google', { successRedirect: '/Menu', failureRedirect: '/Login' }));
+}
+
+if (process.env.GITHUB_APP_ID !== undefined) {
+    passport.use(new GithubStrategy({
+            clientID: process.env.GITHUB_APP_ID,
+            clientSecret: process.env.GITHUB_APP_SECRET,
+            callbackURL: process.env.LOGIN_URL + "/callback/github",
+    },
+        async function(accessToken, refreshToken, profile, done) {
+            console.log(profile);
+            try {
+                console.log(profile)
+                let user = await QueryUser.authGithub(profile.id, profile.username);
+                if(user.username === undefined) {
+                    console.log("[github] authGithub missing user: undefined.")
+                    return done(null, false);
+                }
+                return done(null, user);
+                // return done(null, {username: "123aaa"});
+            }
+            catch (err) {
+                console.log("[ggithub] unexpected error: " + err)
+                return done(null, false)
+            }
+        }
+    ))
+    router.get('/github', passport.authenticate('github', { scope:[ 'profile' ] }));
+
+    router.get('/callback/github', passport.authenticate('github', { successRedirect: '/Menu', failureRedirect: '/Login' }));
 }
 
 passport.serializeUser(function (user, done) {
